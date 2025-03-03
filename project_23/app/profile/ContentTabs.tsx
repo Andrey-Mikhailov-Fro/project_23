69463307a4c5ac5eace8aad2bd48ccd95edda1b1
Andrey-Mikhailov-Fro <dark.ace.frost@gmail.com>
2025-03-03 12:47:22 +0300

    prettified

--- project_23/app/profile/ContentTabs.tsx
+++ project_23/app/profile/ContentTabs.tsx
@@ -1,46 +1,58 @@
-'use client'
+"use client";
 
 import React, { useState } from "react";
 
 export default function ContentTabs() {
-    enum activeTypes {
-        Photos = 'photos',
-        Likes = 'likes'
-    };
+  enum activeTypes {
+    Photos = "photos",
+    Likes = "likes",
+  }
 
-    const photosList = <div>Photos Content</div>;
-    const likesList = <div>Likes Content</div>;
+  const photosList = <div>Photos Content</div>;
+  const likesList = <div>Likes Content</div>;
 
-    const activeContent = {
-        photos: photosList,
-        likes: likesList,
-    };
+  const activeContent = {
+    photos: photosList,
+    likes: likesList,
+  };
 
-    const [active, setActive] = useState(activeTypes.Photos);
+  const [active, setActive] = useState(activeTypes.Photos);
 
-    const activeClass = 'text-blue-700 border-b-2 grow border-blue-700 mt-2 px-8 transition-all duration-500';
-    const nonActiveClass = 'text-gray-500 border-b-2 border-gray-500 mt-2 px-8 transition-all duration-500';
+  const activeClass =
+    "text-blue-700 border-b-2 grow border-blue-700 mt-2 px-8 transition-all duration-500";
+  const nonActiveClass =
+    "text-gray-500 border-b-2 border-gray-500 mt-2 px-8 transition-all duration-500";
 
-    const tabsActiveButtonClasses = {
-        photos: 'photos' === active ? activeClass : nonActiveClass,
-        likes: 'likes' === active ? activeClass : nonActiveClass,
-    };
+  const tabsActiveButtonClasses = {
+    photos: "photos" === active ? activeClass : nonActiveClass,
+    likes: "likes" === active ? activeClass : nonActiveClass,
+  };
 
-    const handleClick = (tabName: activeTypes) => () => {
-        setActive(tabName);
-    };
+  const handleClick = (tabName: activeTypes) => () => {
+    setActive(tabName);
+  };
 
-    function getContent<T, K extends keyof T>(obj: T, prop: K): T[K] {
-        return obj[prop];
-    };
-    
-    return (
-        <>
-            <div className='flex w-full justify-between'>
-                <button onClick={handleClick(activeTypes.Photos)} className={tabsActiveButtonClasses.photos}>Photos</button>
-                <button onClick={handleClick(activeTypes.Likes)} className={tabsActiveButtonClasses.likes}>Likes</button>
-            </div>
-            <div className='flex grow'>{getContent(activeContent, active)}</div>
-        </>
-    );
+  function getContent<T, K extends keyof T>(obj: T, prop: K): T[K] {
+    return obj[prop];
+  }
+
+  return (
+    <>
+      <div className="flex w-full justify-between">
+        <button
+          onClick={handleClick(activeTypes.Photos)}
+          className={tabsActiveButtonClasses.photos}
+        >
+          Photos
+        </button>
+        <button
+          onClick={handleClick(activeTypes.Likes)}
+          className={tabsActiveButtonClasses.likes}
+        >
+          Likes
+        </button>
+      </div>
+      <div className="flex grow">{getContent(activeContent, active)}</div>
+    </>
+  );
 }
--- project_23/app/profile/NavBar.tsx
+++ project_23/app/profile/NavBar.tsx
@@ -5,30 +5,34 @@ import OptionsIcon from "./icons/OptionsIcon";
 import ProfileIcon from "./icons/ProfileIcon";
 
 export default function NavBar() {
-    return (
-        <nav className='absolute flex justify-between max-sm:w-full bottom-0 left-0 md:h-2/3'>
-            <div className='flex relative w-full flex-row md:flex-col md:flex-nowrap justify-around max-sm:mt-5 border-t-2 border-gray-200
-                            max-sm:before:absolute max-sm:before:left-1/2 max-sm:before:-translate-x-1/2 max-sm:before:-top-4 max-sm:before:w-1/6 max-sm:before:aspect-square max-sm:before:shadow-md max-sm:before:shadow-gray-500 max-sm:before:bg-white max-sm:before:rounded-full'>
-                <div className='flex grow max-sm:mr-10 md:flex-col'>
-                    <button className='flex grow justify-center items-center rounded-md transition-colors hover:bg-gray-600 duration-300'>
-                        <HomeIcon />
-                    </button>
-                    <button className='flex grow justify-center items-center rounded-md transition-colors hover:bg-gray-600 duration-300'>
-                        <MessagesIcon />
-                    </button>
-                </div>
-                <div className='flex absolute items-center justify-center -bottom-3 left-1/2 -translate-x-1/2  w-1/6 md:relative md:grow'>
-                    <button className='flex w-full justify-center items-center transition-all hover:-translate-y-4 rounded-full aspect-square bg-blue-700 text-lg text-white'>+</button>
-                </div>
-                <div className='flex grow max-sm:ml-10 md:flex-col'>
-                    <button className='flex grow justify-center items-center rounded-md transition-colors hover:bg-gray-600 duration-300'>
-                        <OptionsIcon />
-                    </button>
-                    <button className='flex grow justify-center items-center rounded-md transition-colors hover:bg-gray-600 duration-300'>
-                        <ProfileIcon />
-                    </button>
-                </div>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="absolute flex justify-between max-sm:w-full bottom-0 left-0 md:h-2/3">
+      <div
+        className="flex relative w-full flex-row md:flex-col md:flex-nowrap justify-around max-sm:mt-5 border-t-2 border-gray-200
+                            max-sm:before:absolute max-sm:before:left-1/2 max-sm:before:-translate-x-1/2 max-sm:before:-top-4 max-sm:before:w-1/6 max-sm:before:aspect-square max-sm:before:shadow-md max-sm:before:shadow-gray-500 max-sm:before:bg-white max-sm:before:rounded-full"
+      >
+        <div className="flex grow max-sm:mr-10 md:flex-col">
+          <button className="flex grow justify-center items-center rounded-md transition-colors hover:bg-gray-600 duration-300">
+            <HomeIcon />
+          </button>
+          <button className="flex grow justify-center items-center rounded-md transition-colors hover:bg-gray-600 duration-300">
+            <MessagesIcon />
+          </button>
+        </div>
+        <div className="flex absolute items-center justify-center -bottom-3 left-1/2 -translate-x-1/2  w-1/6 md:relative md:grow">
+          <button className="flex w-full justify-center items-center transition-all hover:-translate-y-4 rounded-full aspect-square bg-blue-700 text-lg text-white">
+            +
+          </button>
+        </div>
+        <div className="flex grow max-sm:ml-10 md:flex-col">
+          <button className="flex grow justify-center items-center rounded-md transition-colors hover:bg-gray-600 duration-300">
+            <OptionsIcon />
+          </button>
+          <button className="flex grow justify-center items-center rounded-md transition-colors hover:bg-gray-600 duration-300">
+            <ProfileIcon />
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
 }
